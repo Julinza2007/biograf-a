ce81db95b6ca8d9e0cf90201f44e343cfc4edd30
Julian <example@example.com>
2024-07-07 02:51:33 -0300

    Terminé creo que la pagina de mi biografía, bastante bueno está esto de javascript. Sinceramente me está gustando bastante pero bueno, ahora tengo una prueba este lunes y tengo que seguir estudiando

--- main.js
+++ main.js
@@ -15,11 +15,10 @@ document.addEventListener('click', handleClick);
                 event.target.remove();
             }
 
-            if (event.target.id == 'btnContinuar') {
-                let pregunta = document.createElement('p');
+            if(event.target.id == 'btnContinuar') {
+                let pregunta = document.querySelector('#pregMes');
                 pregunta.textContent = `${name}, a ver si me conocés bien... ¿En qué mes nací?`;
                 pregunta.classList.add('pregunta');
-                document.querySelector('#parrafoBiogra').append(pregunta);
                 event.target.remove();
 
                 let boton1 = document.createElement('button');
@@ -46,12 +45,13 @@ document.addEventListener('click', handleClick);
                     resultado.textContent = `La respuesta es correcta!!! dame un besito. Nah mentira me gustan las mujeres.`;
                     let videoContainer = document.querySelector('#videoContainer');
                     videoContainer.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/IMfAOTmnyp4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-                
-                    let preg2 = document.createElement('p');
-                    preg2.textContent = `A ver ${name}, si sos tan crack... ¿Cuál es mi color favorito?`;
-                    preg2.classList.add('pregunta');
-                    videoContainer.append(preg2);
+                    document.querySelector('#botones').remove();
 
+                    let contenedorBtnsColores= document.querySelector('#botonesColor');
+                    let preg2 = document.querySelector('#pregColor');
+                    preg2.classList.add('pregunta');
+                    preg2.textContent = `A ver ${name}, si sos tan crack... ¿Cuál es mi color favorito?`;
+                    
                     let boton1Color = document.createElement('button');
                     let boton2Color = document.createElement('button');
                     let boton3Color = document.createElement('button');
@@ -64,23 +64,36 @@ document.addEventListener('click', handleClick);
                     boton2Color.classList.add('btnPreg2');
                     boton3Color.classList.add('btnPreg2');
                     boton4Color.classList.add('btnPreg2');
-                    videoContainer.append(boton1Color);
-                    videoContainer.append(boton2Color);
-                    videoContainer.append(boton3Color);
-                    videoContainer.append(boton4Color);
+                    boton1Color.style.background = "salmon";
+                    boton2Color.style.background = "rgb(155, 155 ,255)";
+                    boton3Color.style.background = "rgb(95, 220, 234)";
+                    boton4Color.style.background = "rgb(130, 130, 130)";
+                    contenedorBtnsColores.append(boton1Color);
+                    contenedorBtnsColores.append(boton2Color);
+                    contenedorBtnsColores.append(boton3Color);
+                    contenedorBtnsColores.append(boton4Color);    
+                }
 
-                    if(event.target.classList.contains('btnPreg2')){
-                        let resultadoColor = document.querySelector('#resultadoColor');
-                        if(event.target.textContent == 'Azul'){
-                            resultadoColor.textContent = `Muy bien, ${name}, ¿Cómo es que sabés tanto de mí? Nah mentira no sabés un carajo... Hay más cosas pero no puedo ponerlas acá...`;
-                        } else{
-                            resultadoColor.textContent = `MUY MAL FLACO encima te llamas ${name}, que nombre de pelotudo mamita querida. Me caes muy mal ahora, te odio.`;
-                        }
-                    }
-                } else {
+                else{
                     let videoContainer = document.querySelector('#videoContainer');
                     videoContainer.innerHTML = '';
                     resultado.textContent = `La respuesta es incorrecta. Está bien nunca te quise igual :(`;
                 }
+
             }
+
+            if(event.target.classList.contains('btnPreg2')){
+                let resultadoColor = document.querySelector('#resultadoColor');
+                if(event.target.textContent == 'Azul'){
+                    resultadoColor.textContent = `Muy bien, ${name}, ¿Cómo es que sabés tanto de mí? Nah mentira no sabés un carajo... Hay más cosas pero no puedo ponerlas acá...`;
+                    document.querySelector('#botonesColor').remove();
+                    let imagen = document.querySelector('#imagen');
+                    imagen.style.textAlign= "center";
+                    imagen.innerHTML= '<p class="parrafos">Ese soy yo</p><img src="YO.jpg" widht="300px" height="160px">';
+                } else{
+                    resultadoColor.textContent = `MUY MAL FLACO encima te llamas ${name}, que nombre de pelotudo mamita querida. Me caes muy mal ahora, te odio.`;
+                    document.querySelector('#botonesColor').remove();
+                }
+            }
+
         }
